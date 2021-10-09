194e8e6ec453900ec0882f5fee48a4ea9457fb32
Martín Suárez <ms.prgramador@gmail.com>
2021-10-09 17:52:24 +0000

    doing

--- src/models.py
+++ src/models.py
@@ -8,23 +8,41 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+    class Character(Base):
+        __tablename__ = 'character'
+        id = Column(Integer, primary_key=True)
+        name = Column(String(250))
+        height = Column(String(250))
+        mass = Column(String(250))
+        hair_color = Column(String(250))
+        eye_color = Column(String(250))
+        birth_year = Column(String(250))
+        gender = Column(String(250))
+        description = Column(String(250))
+
+
+    class Planet(Base):
+        __tablename__ = 'planet'
+        id = Column(Integer, primary_key=True)
+
 
     def to_dict(self):
         return {}
